1e01121c78787e7564ddd4c0bb64cf016796abfc
endodo <119612909+endo-do@users.noreply.github.com>
2024-01-10 22:23:27 +0100

    Update Table.py
    - Removed not functions replace_cell and remove_row.
    - They will be added along with a remove_column function in the future
    - Layout

--- Table.py
+++ Table.py
@@ -4,13 +4,14 @@ def sort_dict(d):
 
 def restructure(data, structure, fill_with_empty_columns, fill_with_empty_rows, empty_dicts, empty_lists, empty_cells, replace_empty):
     str1 = type(data) 
+    
     if str1 is list:
         str2 = type(data[0]) 
+    
     elif str1 is dict:
         str2 = type(next(iter(data.values()))) 
     
-    if structure == "list":
-        
+    if structure == "list":    
         data_structure = str1.__name__
 
         if data_structure == "dict":            
@@ -22,72 +23,98 @@ def restructure(data, structure, fill_with_empty_columns, fill_with_empty_rows, 
                         data[i] = ""
 
         data = dict(sorted(data.items()))
-        
         data = [i for i in list(data.values())]
             
-
         return data
     
     if structure == "list_in_list":
-        
         data_structure = f"{str2.__name__}_in_{str1.__name__}"
 
         if data_structure == "dict_in_list":
+            
             if fill_with_empty_rows:
                 rows = [key for d in data for key in d]
                 for row in range(rows[0], rows[-1]):
                     if row not in rows:
                         data.append({row:[]})
+            
             new_content = []
             data = sorted(data, key=lambda d: next(iter(d)))
+            
             for line in data:
                 
                 if any(str(val) in empty_dicts for val in line.values()):
                     new_content.append([""])
+                
                 else:
                     new_content.extend([char for char in line.values()])
+            
             data = new_content
 
         elif data_structure == "list_in_dict":
+            
             if fill_with_empty_rows:
                 rows = list(data.keys())
+            
                 for row in range(rows[0], rows[-1]):
                     if row not in rows:
                         data[row] = []
+            
             new_content = []
             data = dict(sorted(data.items()))
+            
             for line in data.values():
+            
                 if line in empty_lists:
                     new_content.append(replace_empty)
+            
                 else:
                     new_content.append(line)
+            
             data = new_content
 
         elif data_structure == "dict_in_dict":
+            
             if fill_with_empty_rows:
                 rows = list(data.keys())
+            
                 for row in range(rows[0], rows[-1]):
+            
                     if row not in rows:
                         data[row] = {}
+            
             if fill_with_empty_columns:
+            
                 for key, row in data.items():
                     columns = list(row.keys())
+            
                     if len(columns) >= 2:
+            
                         for col in range(columns[0], columns[-1]):
+            
                             if col not in columns:
                                 data[key][col] = ""
+            
             new_content = []
             data = {k: dict(sorted(v.items())) if isinstance(v, dict) else v for k, v in sorted(data.items())}
+            
             for line in data:
+            
                 if data[line] in empty_dicts:
                     new_line = [""]
+            
                 else:
                     new_line = []
+            
                     for cell in data[line].values():
+            
                         if str(cell) in empty_cells:
                             cell = replace_empty
+            
                         new_line.append(cell)
+            
                 new_content.append(new_line)
+            
             data = new_content
 
         return data
@@ -120,56 +147,49 @@ class Table:
 
    
     def add_row(self, index, row):
+        
         if "row" in self.header:
             index += 1
+        
         row = restructure(row, "list", self.fill_with_empty_columns, self.fill_with_empty_rows, self.empty_dicts, self.empty_lists, self.empty_cells, self.replace_empty)
+        
         if "col" in self.header:
             row.insert(0, "")
+        
         self.content.insert(index if index != "end" else len(self.content), row)
         self.header_action_row = "update"
         self.header_action_col = "update"
 
    
-    def remove_row(self, index):
-        if "row" in self.header:
-            index += 1
-        self.content.pop(index)
-        self.header_action_row = "update"
-        self.header_actio_col = "update"
-
-   
     def add_column(self, index, column):
+        
         if "col" in self.header:
             index += 1
+        
         column = restructure(column, "list", self.fill_with_empty_columns, self.fill_with_empty_rows, self.empty_dicts, self.empty_lists, self.empty_cells, self.replace_empty)
+        
         if "row" in self.header:
             column.insert(0, "")
+        
         for i in range(len(column)):
             self.content[i].insert(index if index != "end" else len(self.content[i]), column[i])
-        self.header_action_col = "update"
-        self.header_action_row = "update"
-
-   
-    def replace_cell(self, row, col, replace=None):
-        if "row" in self.header:
-            row += 1
-        if "col" in self.header:
-            col += 1
-        self.content[row][col] = self.replace_empty if replace == None else replace
-
-   
-    def swap_cols_rows(self):
         
         self.header_action_col = "update"
         self.header_action_row = "update"
 
+
+    def swap_cols_rows(self):
+        self.header_action_col = "update"
+        self.header_action_row = "update"
         self.content = list(map(list, zip(*self.content)))
 
     
     def change_header(self, headers, type):
         self.header_action = "update"
+        
         if type == "row":
             self.og_header_row = headers
+        
         elif type == "col":
             self.og_header_col = headers
         
@@ -178,11 +198,15 @@ class Table:
     
     
     def main(self):
+        
         if self.orientation not in ["left", "right"]:
             self.orientation = "left"
+        
         for arg in [self.header, self.fill_with_empty_columns, self.fill_with_empty_rows]:
+        
             if arg != True and arg != False:
                 arg = False
+        
         self.content = restructure(self.content, "list_in_list", self.fill_with_empty_columns, self.fill_with_empty_rows, self.empty_dicts, self.empty_lists, self.empty_cells, self.replace_empty)
 
         if "row" in self.header:
@@ -195,17 +219,22 @@ class Table:
             if self.header_action_row == "insert":
                 
                 for row in self.content:
+                    
                     if len(row) > self.columns: 
                         self.columns = len(row)
 
                 if self.header["row"] == []:
+                    
                     if "col" in self.header:
                         self.header["row"] = [f"{index+1}." for index in range(0, self.columns)]
+                    
                     else:
                         self.header["row"] = [f"{index+1}." for index in range(0, self.columns)]
                 
                 else: 
+                    
                     if len(self.header["row"]) < self.columns:
+                    
                         for i in range(len(self.content) - len(self.header["row"])):
                             self.header["row"].append("")
                 
@@ -214,19 +243,27 @@ class Table:
         if "col" in self.header:
             
             if self.header_action_col == "update":
+            
                 for i in self.content:
                     i.pop(0) if i != self.content[0] else i.pop(-1)
+            
                 self.header["col"] = self.og_header_col
                 self.header_action_col = "insert"
             
             if self.header_action_col == "insert":
+            
                 if self.header["col"] == []:
+            
                     if "row" in self.header:
                         self.header["col"] = [""] + [f"{index+1}." for index in range(0, len(self.content) - 1)]
+            
                     else:
                         self.header["col"] = [""] + [f"{index+1}." for index in range(0, len(self.content))]
+            
                 else:
+
                     if len(self.header["col"]) < len(self.content):
+            
                         for i in range(len(self.content) - len(self.header["col"])):
                             self.header["col"].append("")
                 
@@ -238,32 +275,42 @@ class Table:
         
         self.rows = len(self.content)
         for row in self.content:
+            
             if len(row) > self.columns: 
                 self.columns = len(row)
 
         for index, row in enumerate(self.content):
+            
             if row == []:
+            
                 for i in range(self.columns):
                     self.content[index] = ["" for i in range(self.columns)]
 
         for row in self.content:
+            
             while len(row) < self.columns:
                 row.append("")
 
         new_content = []
+        
         for line in self.content: 
             new_content.append([str(char) if char not in self.empty_cells else self.replace_empty for char in line])
+        
         self.content = new_content 
 
         self.max_chars = []
+        
         for cell in range(self.columns): 
             self.max_chars.append(0)
 
         for row in self.content: 
             active_column = 0
+        
             for cell in row:
+        
                 if len(str(cell)) > self.max_chars[active_column]: 
                     self.max_chars[active_column] = len(str(cell))
+        
                 active_column += 1
 
         column_index = 0  
@@ -273,11 +320,15 @@ class Table:
             print("═" * self.space_left, end="")  
             print("═" * column, end="")  
             print("═" * self.space_right, end="")  
+            
             if column_index == len(self.max_chars) - 1:  
                 print("╗")
+            
             else:
+            
                 if "col" in self.header  and column_index == 0:
                     print("╦", end="")
+            
                 else:
                     print("╤", end="")  
             
@@ -287,7 +338,6 @@ class Table:
         
         for row in range(self.rows): 
             print("║", end="") 
-            
             column_index = 0
 
             for column in range(self.columns):
@@ -296,6 +346,7 @@ class Table:
 
                 if self.orientation == "left": 
                     content = text + str(spacebar_counter * " ")  
+                
                 elif self.orientation == "right":
                     content = str(spacebar_counter * " ") + text 
 
@@ -305,9 +356,12 @@ class Table:
                 
                 if column_index == self.columns - 1: 
                     print("║") 
+                
                 else:
+                
                     if "col" in self.header and column_index == 0:
                         line = "║"
+                
                     else:
                         line = "│" 
                     
@@ -334,24 +388,29 @@ class Table:
                 cross_connection = "┼"
 
             print(left_border, end="") 
-                
             column_index = 0
 
             for column in self.max_chars: 
-                
                 print(connection * self.space_left, end="")
                 print(column * connection, end="") 
                 print(connection * self.space_right, end="") 
+                
                 if column_index == len(self.max_chars) - 1: 
                     print(right_border)
+                
                 else:
+                
                     if "col" in self.header and column_index == 0:
+                
                         if row_index == self.rows - 1:
                             print("╩", end="")
+                
                         elif "row" in self.header and row_index == 0:
                             print("╬", end="")
+                
                         else:
                             print("╫", end="")
+                
                     else:
                         print(cross_connection, end="") 
 
