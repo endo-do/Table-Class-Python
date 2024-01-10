b595f42ea61b2dc051777517fd9b21d4f5710652
endodo <119612909+endo-do@users.noreply.github.com>
2024-01-10 22:13:32 +0100

    Update Table.py
    - Fixed the header mechanism when adding rows/cols
    - Fixed the header mechanism when swapping rows with cols and vice versa
    - added "end" for possible index var when adding rows/cols
    
    -> removing rows/cols will be added in the future

--- Table.py
+++ Table.py
@@ -2,80 +2,102 @@ def sort_dict(d):
    return {k: sort_dict(v) if isinstance(v, dict) else v for k, v in sorted(d.items())}
 
 
-def restructure_to_listinlist(data, fill_with_empty_columns, fill_with_empty_rows, empty_dicts, empty_lists, empty_cells, replace_empty):
+def restructure(data, structure, fill_with_empty_columns, fill_with_empty_rows, empty_dicts, empty_lists, empty_cells, replace_empty):
     str1 = type(data) 
     if str1 is list:
         str2 = type(data[0]) 
     elif str1 is dict:
         str2 = type(next(iter(data.values()))) 
-    data_structure = f"{str2.__name__}_in_{str1.__name__}"
     
-    if data_structure == "dict_in_list":
-        if fill_with_empty_rows:
-            rows = [key for d in data for key in d]
-            for row in range(rows[0], rows[-1]):
-                if row not in rows:
-                    data.append({row:[]})
-        new_content = []
-        data = sorted(data, key=lambda d: next(iter(d)))
-        for line in data:
-            
-            if any(str(val) in empty_dicts for val in line.values()):
-                new_content.append([""])
-            else:
-                new_content.extend([char for char in line.values()])
-        data = new_content
+    if structure == "list":
+        
+        data_structure = str1.__name__
 
-    elif data_structure == "list_in_dict":
-        if fill_with_empty_rows:
-            rows = list(data.keys())
-            for row in range(rows[0], rows[-1]):
-                if row not in rows:
-                    data[row] = []
-        new_content = []
+        if data_structure == "dict":            
+            columns = list(data.keys())
+
+            if len(columns) >= 2:
+                for i in range(columns[0], columns[-1]):
+                    if i not in columns:
+                        data[i] = ""
+
         data = dict(sorted(data.items()))
-        for line in data.values():
-            if line in empty_lists:
-                new_content.append(replace_empty)
-            else:
-                new_content.append(line)
-        data = new_content
+        
+        data = [i for i in list(data.values())]
+            
 
-    elif data_structure == "dict_in_dict":
-        if fill_with_empty_rows:
-            rows = list(data.keys())
-            for row in range(rows[0], rows[-1]):
-                if row not in rows:
-                    data[row] = {}
-        if fill_with_empty_columns:
-            for key, row in data.items():
-                columns = list(row.keys())
-                if len(columns) >= 2:
-                    for col in range(columns[0], columns[-1]):
-                        if col not in columns:
-                            data[key][col] = ""
-        new_content = []
-        data = {k: dict(sorted(v.items())) if isinstance(v, dict) else v for k, v in sorted(data.items())}
-        for line in data:
-            if data[line] in empty_dicts:
-                new_line = [""]
-            else:
-                new_line = []
-                for cell in data[line].values():
-                    if str(cell) in empty_cells:
-                        cell = replace_empty
-                    new_line.append(cell)
-            new_content.append(new_line)
-        data = new_content
+        return data
+    
+    if structure == "list_in_list":
+        
+        data_structure = f"{str2.__name__}_in_{str1.__name__}"
 
-    return data
+        if data_structure == "dict_in_list":
+            if fill_with_empty_rows:
+                rows = [key for d in data for key in d]
+                for row in range(rows[0], rows[-1]):
+                    if row not in rows:
+                        data.append({row:[]})
+            new_content = []
+            data = sorted(data, key=lambda d: next(iter(d)))
+            for line in data:
+                
+                if any(str(val) in empty_dicts for val in line.values()):
+                    new_content.append([""])
+                else:
+                    new_content.extend([char for char in line.values()])
+            data = new_content
+
+        elif data_structure == "list_in_dict":
+            if fill_with_empty_rows:
+                rows = list(data.keys())
+                for row in range(rows[0], rows[-1]):
+                    if row not in rows:
+                        data[row] = []
+            new_content = []
+            data = dict(sorted(data.items()))
+            for line in data.values():
+                if line in empty_lists:
+                    new_content.append(replace_empty)
+                else:
+                    new_content.append(line)
+            data = new_content
+
+        elif data_structure == "dict_in_dict":
+            if fill_with_empty_rows:
+                rows = list(data.keys())
+                for row in range(rows[0], rows[-1]):
+                    if row not in rows:
+                        data[row] = {}
+            if fill_with_empty_columns:
+                for key, row in data.items():
+                    columns = list(row.keys())
+                    if len(columns) >= 2:
+                        for col in range(columns[0], columns[-1]):
+                            if col not in columns:
+                                data[key][col] = ""
+            new_content = []
+            data = {k: dict(sorted(v.items())) if isinstance(v, dict) else v for k, v in sorted(data.items())}
+            for line in data:
+                if data[line] in empty_dicts:
+                    new_line = [""]
+                else:
+                    new_line = []
+                    for cell in data[line].values():
+                        if str(cell) in empty_cells:
+                            cell = replace_empty
+                        new_line.append(cell)
+                new_content.append(new_line)
+            data = new_content
+
+        return data
 
 
 
 class Table:
     def __init__(self, content, space_left=1, space_right=1, orientation="left", 
                 empty_cells=["", "#empty"], empty_lists=["", "#empty"], empty_dicts=["", "#empty"], replace_empty="",
-                header={"row":[]}, fill_with_empty_rows=False, fill_with_empty_columns=False):
+                header={"row":[]}, fill_with_empty_rows=True, fill_with_empty_columns=True):
         
         self.content = content 
         self.space_left = space_left 
@@ -93,32 +115,39 @@ class Table:
         self.og_header_row = self.header["row"] if "row" in self.header else []
         self.rows = 0 
         self.columns = 0  
-        self.header_action = "insert"    
+        self.header_action_col = "insert"
+        self.header_action_row = "insert"    
 
    
     def add_row(self, index, row):
         if "row" in self.header:
             index += 1
+        row = restructure(row, "list", self.fill_with_empty_columns, self.fill_with_empty_rows, self.empty_dicts, self.empty_lists, self.empty_cells, self.replace_empty)
         if "col" in self.header:
             row.insert(0, "")
-        row = [row]
-        self.content.insert(index, restructure_to_listinlist(row, self.fill_with_empty_columns, self.fill_with_empty_rows, self.empty_dicts, self.empty_lists, self.empty_cells, self.replace_empty)[0])
-        self.header_action = "update"
+        self.content.insert(index if index != "end" else len(self.content), row)
+        self.header_action_row = "update"
+        self.header_action_col = "update"
 
    
     def remove_row(self, index):
         if "row" in self.header:
             index += 1
         self.content.pop(index)
-        self.header_action = "update"
+        self.header_action_row = "update"
+        self.header_actio_col = "update"
 
    
     def add_column(self, index, column):
+        if "col" in self.header:
+            index += 1
+        column = restructure(column, "list", self.fill_with_empty_columns, self.fill_with_empty_rows, self.empty_dicts, self.empty_lists, self.empty_cells, self.replace_empty)
         if "row" in self.header:
             column.insert(0, "")
         for i in range(len(column)):
-            self.content[i].insert(index, column[i])
-        self.header_action = "update"
+            self.content[i].insert(index if index != "end" else len(self.content[i]), column[i])
+        self.header_action_col = "update"
+        self.header_action_row = "update"
 
    
     def replace_cell(self, row, col, replace=None):
@@ -130,12 +159,10 @@ class Table:
 
    
     def swap_cols_rows(self):
-        if "row" in self.header and "col" not in self.header:
-            self.header.pop("row", None)
-            self.header["col"] = self.content[0]
-        elif "col" in self.header and "row" not in self.header:
-            self.header.pop("col", None)
-            self.header["row"] = self.content[0]
+        
+        self.header_action_col = "update"
+        self.header_action_row = "update"
+
         self.content = list(map(list, zip(*self.content)))
 
     
@@ -156,42 +183,58 @@ class Table:
         for arg in [self.header, self.fill_with_empty_columns, self.fill_with_empty_rows]:
             if arg != True and arg != False:
                 arg = False
-        self.content = restructure_to_listinlist(self.content, self.fill_with_empty_columns, self.fill_with_empty_rows, self.empty_dicts, self.empty_lists, self.empty_cells, self.replace_empty)
-           
+        self.content = restructure(self.content, "list_in_list", self.fill_with_empty_columns, self.fill_with_empty_rows, self.empty_dicts, self.empty_lists, self.empty_cells, self.replace_empty)
+
         if "row" in self.header:
-            if self.header_action == "update":
+            
+            if self.header_action_row == "update":
                 self.content.pop(0)
                 self.header["row"] = self.og_header_row
-                self.header_action = "insert"
-            if self.header_action == "insert":
+                self.header_action_row = "insert"
+            
+            if self.header_action_row == "insert":
+                
                 for row in self.content:
                     if len(row) > self.columns: 
-                        self.columns = len(row)  
+                        self.columns = len(row)
+
                 if self.header["row"] == []:
-                    self.header["row"] = [f"{index}." for index in range(self.columns)]
-                if len(self.header["row"]) < self.columns:
-                    for i in range(len(self.content) - len(self.header["row"])):
-                        self.header["row"].append("")
-                self.content = [self.header["row"]] + self.content
+                    if "col" in self.header:
+                        self.header["row"] = [f"{index+1}." for index in range(0, self.columns)]
+                    else:
+                        self.header["row"] = [f"{index+1}." for index in range(0, self.columns)]
                 
+                else: 
+                    if len(self.header["row"]) < self.columns:
+                        for i in range(len(self.content) - len(self.header["row"])):
+                            self.header["row"].append("")
+                
+            self.content = [self.header["row"][:self.columns]] + self.content
+
         if "col" in self.header:
-            if self.header_action == "update":
-                for i in self.content:
-                    i.pop(0)
-                self.header["col"] = self.og_header_col
-                self.header_action = "insert"
-            if self.header_action == "insert":
-                if self.header["col"] == []:
-                    self.header["col"] = [f"{index}." for index in range(len(self.content))]
-                    if "row" in self.header:
-                        self.header["col"] = [""] + self.header["col"]
-                if len(self.header["col"]) < len(self.content):
-                    for i in range(len(self.content) - len(self.header["col"])):
-                        self.header["col"].append("")
-                for index, i in enumerate(self.header["col"]):
-                    self.content[index] = [self.header["col"][index]] + self.content[index]
             
-        self.header_action = "nothing"
+            if self.header_action_col == "update":
+                for i in self.content:
+                    i.pop(0) if i != self.content[0] else i.pop(-1)
+                self.header["col"] = self.og_header_col
+                self.header_action_col = "insert"
+            
+            if self.header_action_col == "insert":
+                if self.header["col"] == []:
+                    if "row" in self.header:
+                        self.header["col"] = [""] + [f"{index+1}." for index in range(0, len(self.content) - 1)]
+                    else:
+                        self.header["col"] = [""] + [f"{index+1}." for index in range(0, len(self.content))]
+                else:
+                    if len(self.header["col"]) < len(self.content):
+                        for i in range(len(self.content) - len(self.header["col"])):
+                            self.header["col"].append("")
+                
+                for index, i in enumerate(self.header["col"]):
+                    self.content[index] = [i] + self.content[index]
+    
+        self.header_action_col = "nothing"
+        self.header_action_row = "nothing"
         
         self.rows = len(self.content)
         for row in self.content:
